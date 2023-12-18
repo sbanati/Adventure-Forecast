c66928cc8ed5c4a1739b67a88a579bb142c7cd65
smbanati-IZxCKm <smbanati@gmail.com>
2023-12-18 16:06:21 -0500

    added function to retrieve city coords from API and making a call to new function getweatherdata. I set up getweatherdata function to fetch weather from city but need to setup 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,56 @@ const weatherCardsDiv = document.querySelector('.weather-cards'); // assigning H
 const searchButton = document.querySelector('.search-btn'); // assigning HTML button element to the const variable with the .search-btn
 const cityInput = document.querySelector('.city-input'); // assigning HTML input element to the const variable with the .city-input
 
+const ApiKey = 'f22020df271a2b65dbf8c5da9ccb2070'; // random generated API key for the OpenWeatherMap API 
 
-// function to get the city coordinates 
+
+
+
+const getWeatherData = function (cityName, lat, lon)  {
+
+    const weatherApiUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${ApiKey}`;
+
+    fetch(weatherApiUrl).then(res => res.json()).then(data => {
+        console.log(data)
+    }).catch(() => {
+        alert('Error occured while fetching the weather forecast')
+
+    })
+
+}; /*!TODO We have the weather details but it gives us incriments of every 3 hours, so my next step is to sort this in a 5 day forecast. */
+
+
+
+
+
+
+
+
+
+
+// function to get the city coordinates and name from the API response 
 const retrieveCityCoordinates = function () {
     const cityName = cityInput.value.trim(); // get the name of the city entered in the box and pass it to cityName const. Input is a value. 
     if(!cityName) return; // if city name is empty, return 
 
     console.log(cityName) // checks if city name entered in box shows a value. 
 
+    const GeocodingApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${ApiKey}`;
+
+    // get the city coordinates and name from the API response
+    fetch(GeocodingApiUrl)
+        .then(res => res.json()).then(data => {
+            // error message incase array data is empty 
+            if(!data.length) return alert(`Coordinates not found for ${cityName}`);
+            // grabbing the specific properties in the array element 0
+            const { name, lat, lon } = data[0];
+            getWeatherData(name, lat, lon);
+        }).catch(() => {
+            alert('Error occured while fetching the coordinates')
+
+        })
+
+    
 };
 
 
@@ -21,3 +63,9 @@ const retrieveCityCoordinates = function () {
 
 // event listener for the click of the search button to call the function retrieveCityCoordinates
 searchButton.addEventListener('click', retrieveCityCoordinates);    
+
+
+
+
+
+
