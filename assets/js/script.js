c70e410bf1ee922696055778c74b10f5838a7853
smbanati-IZxCKm <smbanati@gmail.com>
2023-12-19 19:00:28 -0500

    added display for the main card. Noticed a bug where the current day is not showing so the main card shows the following day and the 5 day forecast shows 4 days. The array is length of 5 , need to find a way to make it array 6. Logic needs to be looked at

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// const currentWeatherDiv = document.querySelector('.current-weather'); // assigning HTML element to the const variable with the .current-weather selector
+const currentWeatherDiv = document.querySelector('.current-weather'); // assigning HTML element to the const variable with the .current-weather selector
 const weatherCardsDiv = document.querySelector('.weather-cards'); // assigning HTML element to the const variable with the .weather-cards
 const searchButton = document.querySelector('.search-btn'); // assigning HTML button element to the const variable with the .search-btn
 const cityInput = document.querySelector('.city-input'); // assigning HTML input element to the const variable with the .city-input
@@ -7,31 +7,39 @@ const cityInput = document.querySelector('.city-input'); // assigning HTML input
 
 const ApiKey = 'f22020df271a2b65dbf8c5da9ccb2070'; // random generated API key for the OpenWeatherMap API 
 
+// function to retrieve day of the week 
+const getDayOfWeek = ( dateString => {
+    const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const date = new Date( dateString); 
+    const dayIndex = date.getDay();
+    return daysOfWeek[dayIndex];
+
+})
 
 
 
 
 
-
-
-const createWeatherCard = (weatherItem) => {
-    
-         // Check if weatherItem has the expected properties
-    if (!weatherItem || !weatherItem.dt_txt || !weatherItem.weather || !weatherItem.weather[0]) {
-        console.error('Invalid weatherItem object:', weatherItem);
-        return '';  // Return an empty string to prevent rendering an invalid card
-
-    }
-    
-    
-    return `<li class="card">
-                <h3>(${weatherItem.dt_txt.split('')[0]})</h3> 
+const createWeatherCard = (cityName, weatherItem, index) => {
+    if(index === 0) { // HTML of the main weather display
+        return ` <div class="details">
+                    <h3>${cityName} (${getDayOfWeek(weatherItem.dt_txt)})</h3> 
+                    <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+                    <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(0)}°C</h4>
+                    <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
+                    <h4>Humidity: ${weatherItem.main.humidity} %</h4>
+                    </div>`;
+    } else {  // HTML for the 5 day forecast card 
+        return `<li class="card">
+                <h3>${getDayOfWeek(weatherItem.dt_txt)}</h3> 
                 <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
-                <h4>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(0)}°C</h4>
+                <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(0)}°C</h4>
                 <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
                 <h4>Humidity: ${weatherItem.main.humidity} %</h4>
                 </li>`;
 
+    }
+    
 };
 
 
@@ -43,10 +51,11 @@ const getWeatherData = (cityName, lat, lon) => {
     // constructed the API URL with param placeholders
     const weatherApiUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${ApiKey}`;
 
+
     //Making Fetch call from API with the URL 
     fetch(weatherApiUrl).then(res => res.json()).then(data => { // fetches response object data and converts to JSON format then processes JSON data so it can be used in code block below.
 
-        // store the individual forecast days into an Array
+        // Getting only one forecast per day 
         const individualForecastDays = [];
 
         // Using the filter method on the list of forecast hours to retrieve unique forecast days
@@ -62,13 +71,24 @@ const getWeatherData = (cityName, lat, lon) => {
                 return individualForecastDays.push(forecastDate);
             }
         });   
+  
+            console.log(fiveDayForecast);
    
+
+            cityInput.value = ''; // clear the previous input value
             weatherCardsDiv.innerHTML = ''; // Clear existing cards content 
+            currentWeatherDiv.innerHTML = ''; // Clear existing cards content 
 
             // Generating a weather card for each forecast day
-            fiveDayForecast.forEach(weatherItem => {
-                console.log('Selected weatherCardsDiv:', weatherCardsDiv);
-                weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(weatherItem));
+            fiveDayForecast.forEach((weatherItem, index) => {
+
+                if(index === 0) {
+                     console.log('Current Day Weather Item:', weatherItem);
+                    currentWeatherDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+                } else {
+                    weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+                }
+                
                 
                 
             });
@@ -79,10 +99,6 @@ const getWeatherData = (cityName, lat, lon) => {
     })
 
 }; 
-
-
-
-
 
 
 
